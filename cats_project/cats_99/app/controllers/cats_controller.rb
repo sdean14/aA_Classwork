8c46d9f613ce7b211ec8921b7ab15e5a13bdebba
Kevin <kevinsebastianmathews@gmail.com>
2021-02-26 17:10:54 -0800

    finished

--- cats_project/cats_99/app/controllers/cats_controller.rb
+++ cats_project/cats_99/app/controllers/cats_controller.rb
@@ -10,7 +10,39 @@ class CatsController < ApplicationController
         render :show
     end
 
+    def edit
+        @kitty=Cat.find(params[:id])
+        render :edit
+    end
 
+    def update
+     @kitty=Cat.find(params[:id]
+     if @kitty.update(cat_params)
+        redirect_to cat_url(@kitty)
+        
+     else
+        render :edit         
+     end
+    end
+
+
+    def create
+        @kitty=Cat.new(cat_params)
+        if @kitty.save
+            redirect_to cat_url(@kitty.id)
+        else
+            render :new
+        end
+       
+    end
+
+    
+    def new
+        @kitty=Cat.new
+        render :new
+    end
+
+    
     private
     def cat_params
         params.require(:cat).permit(:age, :birth_date, :color, :name, :sex, :description)

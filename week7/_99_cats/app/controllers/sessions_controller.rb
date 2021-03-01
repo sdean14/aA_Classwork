6ca216ede71e2fc1af92b9feeec7b36ea9cff70e
lpoole <keithpoole123@gmail.com>
2021-03-01 18:04:46 -0500

    create aplication_controller helper method

--- week7/_99_cats/app/controllers/sessions_controller.rb
+++ week7/_99_cats/app/controllers/sessions_controller.rb
@@ -5,5 +5,16 @@ class SessionsController < ApplicationController
 
     def create
         user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if user 
+          login!(user)
+        else
+          render :new
+        end
+    end
+
+    def destroy
+      # logout!
+      # redirect_to new_session_url
+      
     end
 end

0b43639d64b218bbffee4323dc4fbea67b83def2
lpoole <keithpoole123@gmail.com>
2021-03-01 17:13:54 -0500

    finish user model, add user controller

--- week7/_99_cats/app/models/user.rb
+++ week7/_99_cats/app/models/user.rb
@@ -3,6 +3,10 @@ class User < ApplicationRecord
     validates :password_digest, presence: true
     validates :session_token, presence: true, uniqueness: true
 
+    attr_reader :password
+
+    after_initialize: ensure_session_token
+
     def self.fine_by_credentials(username, password)
         user = User.find_by(username: username)
         if user && user.password?(password)
@@ -20,6 +24,15 @@ class User < ApplicationRecord
         self.password_digest = BCrypt::Password.create(password)
         @password = password
     end
+    
+    def reset_session_token!
+      self.session_token = SecureRandom::urlsafe_base64
+      self.save!
+      self.session_token(return token)
+    end
 
+    def ensure_session_token
+      self.session_token ||= SecureRandom::urlsafe_base64
+    end
     
 end

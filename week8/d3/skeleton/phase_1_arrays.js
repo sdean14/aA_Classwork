f4aec64301e7fa89e689d9cef7c0f8fa8dd545b4
mike <michaeldeanmdmph@gmail.com>
2021-03-10 14:36:21 -0800

    twoSum

--- week8/d3/skeleton/phase_1_arrays.js
+++ week8/d3/skeleton/phase_1_arrays.js
@@ -16,3 +16,19 @@ Array.prototype.uniq = function() {
 
 let arr = [1, 2, 2, 3, 3, 3];
 console.log(arr.uniq())
+
+Array.prototype.twoSum = function() {
+    let arr = [];
+    for (let i = 0; i < this.length; i++) {
+        for (let j= 0; j < this.length; j++) {
+            if (this[i] + this[j] === 0 && j > i ) {
+                arr.push(this[i], this[j]);
+            }
+        }
+    }
+    if (arr[0] + arr[1] === 0) {
+        return true;
+    }
+} 
+ let arr2 = [4, -4, 10];
+ console.log(arr2.twoSum());
